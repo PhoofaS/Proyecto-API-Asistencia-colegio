5ba71624f8d6374471ff000a70a5193c77065e69
Flam <110363578+653398@users.noreply.github.com>
2025-06-25 22:01:40 -0500

    Update ProfesorRepository.java

--- src/main/java/com/isil/edu/pe/repository/ProfesorRepository.java
+++ src/main/java/com/isil/edu/pe/repository/ProfesorRepository.java
@@ -1,5 +1,9 @@
 package com.isil.edu.pe.repository;
 
-public class ProfesorRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import model.ProfesorModel;
+
+public interface ProfesorRepository extends JpaRepository<ProfesorModel,Long> {
 
 }
