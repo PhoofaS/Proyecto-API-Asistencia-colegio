34fef25e1d9a5d4973a525975982b960f1477d78
gibbons06 <olenkagibbonse@gmail.com>
2025-06-25 19:02:03 -0500

    Add files via upload

---
+++ src/main/java/com/isil/edu/pe/repository/AlumnoRepository.java
@@ -0,0 +1,9 @@
+package com.isil.edu.pe.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import model.AlumnoModel;
+
+public interface AlumnoRepository extends JpaRepository<AlumnoModel,Long> {
+
+}
