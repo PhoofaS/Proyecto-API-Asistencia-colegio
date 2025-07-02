418d576fdf3eb86f7fb856d116ce436b3da35d0d
Neoo <96897283+PhoofaS@users.noreply.github.com>
2025-07-02 08:59:26 -0500

    Delete main/java/com/isil/edu/pe/repository/AlumnoRepository.java

--- main/java/com/isil/edu/pe/repository/AlumnoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.isil.edu.pe.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.isil.edu.pe.model.AlumnoModel;
-
-public interface AlumnoRepository extends JpaRepository<AlumnoModel,Long> {
-
-}
